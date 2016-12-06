a4a40ad2766b3bd5f5943aa997f3a8e8e4c57c76
pashitsa <pavel.molkov@mail.ru>
2016-12-06 08:18:26 +0300

    Добавлены stripline + подсчет коэффициентов

---
+++ SpectralWindowDeleter/HorizontalStripline.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Drawing;
+
+namespace SpectralWindowDeleter
+{
+    public class HorizontalStripline : StripLine
+    {
+        SpectralMeasurement figure = new SpectralMeasurement();
+      
+
+        public HorizontalStripline(int i)
+        {
+            this.Interval = 0;
+            this.IntervalOffset = 0.1*i;
+            this.StripWidth = 0.001;
+            this.Tag = i;
+            this.BackColor = Color.LightGray;
+            this.BackHatchStyle = ChartHatchStyle.DarkVertical;
+            
+        }
+    }
+}
